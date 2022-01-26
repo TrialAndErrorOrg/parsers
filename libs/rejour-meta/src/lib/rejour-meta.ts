6a544f2d6bc076ea171e2d2ff74ee0b6cc28818a
Thomas F. K. Jorna <hello@tefkah.com>
2022-01-26 13:01:44 +0100

    fix(meta): postpone metadata for now

--- libs/rejour-meta/src/lib/rejour-meta.ts
+++ libs/rejour-meta/src/lib/rejour-meta.ts
@@ -1,3 +1,18 @@
-export function rejourMeta(): string {
-  return 'rejour-meta'
+import { ArticleMetaType, Root } from '@jote/rejour'
+import { visit } from 'unist-util-visit'
+
+export default function rejourMoveAbstract(options: ArticleMetaType) {
+  return (tree: Root) => {
+    visit(
+      tree,
+      (node) => node.tagName === 'front',
+      (node) => {
+        for (const [tagName, desc] of Object.entries(options)) {
+          // const type = new ArticleMetaType()
+          // const kidsOrProps =  instanceof 'string'? prop : kid
+          // node.children.push({ type: 'element', tagName })
+        }
+      }
+    )
+  }
 }

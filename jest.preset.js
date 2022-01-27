ee014c073148160ecd5d7f17f76c6d71dd9f85b5
Thomas F. K. Jorna <hello@tefkah.com>
2022-01-27 14:22:27 +0100

    chore: remove trace & change jest preset to esm

--- jest.preset.js
+++ jest.preset.js
@@ -2,4 +2,21 @@ const nxPreset = require('@nrwl/jest/preset')
 
 module.exports = {
   ...nxPreset,
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      useESM: true,
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: [],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 }

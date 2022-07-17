bbc256207a93741d552c058dc7ffe10daf4db4cf
Thomas F. K. Jorna <hello@tefkah.com>
2022-07-17 19:15:24 +0200

    make api do multiple formats using ?format=*

--- api/style.rb
+++ api/style.rb
@@ -14,22 +14,24 @@ Handler = Proc.new do |req, res|
         end
 
         ## Get the 'format' parameter from the request query string
-        style = req.query['format']
+        style =/format=(\w+\b)/.match(req.query_string)[1]
 
-
-        parsed = AnyStyle.parse(body,{format: style || 'csl'})
+        # parsed = AnyStyle.parse(body,:style)
 
 
         res.status = 200
         res['Access-Control-Allow-Origin'] = '*'
         ## if then
 
-        if style == 'json' || !style|| style == 'csl'
+        if style == 'json' ||  style == 'csl' || !style
+          pp "whoops"
           res['Content-Type'] = 'application/json; charset=utf-8'
+          parsed = AnyStyle.parse body, format: 'csl'
           res.body = JSON.generate(parsed)
         else
+          parsed = AnyStyle.parse body, format: style
           res['Content-Type'] = 'text/plain; charset=utf-8'
-          res.body = parsed
+          res.body = parsed.to_s
         end
 end
 

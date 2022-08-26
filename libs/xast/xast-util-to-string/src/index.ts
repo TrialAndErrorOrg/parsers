dc35052401aa46fe853611d7755cbef865c6a908
Thomas F. K. Jorna <hello@tefkah.com>
2022-08-26 18:32:17 +0200

    remove xast-util-to-string in favor of canonical lib

--- libs/xast/xast-util-to-string/src/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './lib/xast-util-to-string'
--- libs/xast/xast-util-to-string/src/lib/xast-util-to-string.spec.ts
+++
@@ -1,9 +0,0 @@
-import { toString } from './xast-util-to-string'
-
-describe('xastXastUtilToString', () => {
-  it('should work', () => {
-    expect(toString({ type: 'text', value: 'Hello World' })).toEqual(
-      'Hello World'
-    )
-  })
-})

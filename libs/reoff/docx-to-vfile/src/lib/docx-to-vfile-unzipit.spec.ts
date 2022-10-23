e9cd1611777389591b576503f2d0c7df4e1f9df3
Thomas F. K. Jorna <hello@tefkah.com>
2022-10-23 12:49:40 +0200

    fix some loose files

--- libs/reoff/docx-to-vfile/src/lib/docx-to-vfile-unzipit.spec.ts
+++ libs/reoff/docx-to-vfile/src/lib/docx-to-vfile-unzipit.spec.ts
@@ -13,18 +13,26 @@ describe('reoffDocxToVfile', () => {
   jest.setTimeout(10000)
   it('should work', async () => {
     const vfile = await docxToVFile(new Uint8Array(doc))
-    fs.writeFileSync(new URL('test', import.meta.url), String(vfile))
+    fs.writeFileSync(
+      new URL('../fixtures/test.xml', import.meta.url),
+      String(vfile)
+    )
     expect(vfile).toMatchSnapshot()
   })
   it('should contain vfile with relations data object', async () => {
     const vfile = await docxToVFile(new Uint8Array(doc))
-    fs.writeFileSync(new URL('test', import.meta.url), String(vfile))
+    fs.writeFileSync(
+      new URL('../fixtures/testrelations.xml', import.meta.url),
+      String(vfile)
+    )
     expect(vfile.data.relations).toBeDefined()
   })
   it('should contain images', async () => {
     const vfile = await docxToVFile(new Uint8Array(docimg))
-    // console.log(vfile)
-    fs.writeFileSync(new URL('testimg', import.meta.url), String(vfile))
+    fs.writeFileSync(
+      new URL('../fixtures/testimg.xml', import.meta.url),
+      String(vfile)
+    )
     expect(vfile.data.images).toBeDefined()
   })
 })

f9648ba9947a78a2dc6362f94707fa49f641c488
Thomas F. K. Jorna <hello@tefkah.com>
2023-03-09 23:51:13 +0100

    fix(ooxast-util-to-jast): fix last extension error

--- libs/ooxast/ooxast-util-to-jast/src/lib/handlers/body.spec.ts
+++ libs/ooxast/ooxast-util-to-jast/src/lib/handlers/body.spec.ts
@@ -1,31 +1,24 @@
 import { x } from 'xastscript'
-import { toJast } from '../ooxast-util-to-jast'
+import { toJast } from '../ooxast-util-to-jast.js'
 import fs from 'fs'
-import { removePosition } from 'unist-util-remove-position'
 
 it('should work', () => {
   const doc = x('w:document', {}, [
     x('w:body', {}, [
       x('w:p', {}, [
         x('w:pPr', {}, [x('w:pStyle', { 'w:val': 'Heading1' })]),
-        x('w:r', {}, [
-          x('w:rPr', {}, []),
-          x('w:t', {}, [{ type: 'text', value: 'hey' }]),
-        ]),
+        x('w:r', {}, [x('w:rPr', {}, []), x('w:t', {}, [{ type: 'text', value: 'hey' }])]),
       ]),
       x('w:p', {}, []),
     ]),
   ])
   expect(toJast(doc)).toEqual(
-    x('body', x('sec', {}, [x('title', {}, [{ type: 'text', value: 'hey' }])]))
+    x('body', x('sec', {}, [x('title', {}, [{ type: 'text', value: 'hey' }])])),
   )
 })
 
 it('lets try', () => {
-  const ooxloc = new URL(
-    '../../../../../reoff/reoff-parse/src/test/ooxasttree',
-    import.meta.url
-  )
+  const ooxloc = new URL('../../../../../reoff/reoff-parse/src/test/ooxasttree', import.meta.url)
   const x = fs.readFileSync(ooxloc, { encoding: 'utf-8' })
   const tree = JSON.parse(x)
 

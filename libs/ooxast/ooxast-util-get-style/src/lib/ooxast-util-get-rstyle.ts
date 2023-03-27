27612f22da9f25a3c6a120b7b1d929f3da6bc28e
Thomas F. K. Jorna <hello@tefkah.com>
2023-03-27 22:43:35 +0200

    feat(ooxast-util-get-style): improve

--- libs/ooxast/ooxast-util-get-style/src/lib/ooxast-util-get-rstyle.ts
+++ libs/ooxast/ooxast-util-get-style/src/lib/ooxast-util-get-rstyle.ts
@@ -5,22 +5,21 @@ import { convertElement } from 'xast-util-is-element'
 const isRPr = convertElement<RPr>('w:rPr')
 export type RPrAttributes = keyof RPrMap
 export type RPrJSON = RPrAttributes extends keyof RPrMap
-  ? Partial<
-      Record<
-        `w:${RPrAttributes}`,
-        RPrMap[RPrAttributes] | { 'w:val'?: boolean }
-      >
-    >
+  ? Partial<Record<`w:${RPrAttributes}`, RPrMap[RPrAttributes] | { 'w:val'?: boolean }>>
   : never
 
-export function getRStyle(r: R): RPrJSON {
-  const style = select('w\\:rPr', r)
-  if (!isRPr(style)) return {}
+export function getRStyle(r: R | P): RPrJSON | undefined {
+  const style = convertElement<P>('w:p')(r) ? select('w\\:pPr > w\\:rPr', r) : select('w\\:rPr', r)
 
+  if (!isRPr(style)) return undefined
+
+  return rPrToJson(style)
+}
+
+function rPrToJson(style: RPr) {
   return style.children.reduce((acc, curr) => {
     if (!curr.name) return acc
 
-    // const { ['w:val']: val, ...attrs } = curr?.attributes
     acc[curr.name] = { ...curr?.attributes, 'w:val': true }
 
     return acc

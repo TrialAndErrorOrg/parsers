4b24aac3d139d769fdd4958aa903bb0a18f98abf
Thomas F. K. Jorna <hello@tefkah.com>
2023-03-27 22:42:02 +0200

    fix(reoff-markup-to-style): create reoff plugin for inferring styles

---
+++ libs/reoff/reoff-markup-to-style/src/index.ts
@@ -0,0 +1,1 @@
+export { default, Options } from './lib/reoff-markup-to-style.js'
---
+++ libs/reoff/reoff-markup-to-style/src/lib/reoff-markup-to-style.ts
@@ -0,0 +1,50 @@
+import { Root } from 'ooxast'
+import { markupToStyle, Options } from 'ooxast-util-markup-to-style'
+import { Plugin } from 'unified'
+export { Options }
+
+/**
+ * `reoff` plugin for inferring paragraph styles from the markup of the underlying text.
+ *
+ * By default, this plugin will infer the following styles:
+ * - `w:b` -> `Heading 1`
+ * - `w:i` -> `Heading 2`
+ *
+ * This plugin can be configured to infer other styles as well.
+ *
+ * @example
+ * ```ts
+ * const options: Options = [
+ * // find all paragraphs that are bold and larger than 24pt and make it a Title
+ *  {
+ *   markup: (rPr) => rPr['w:b'] && rPr['w:sz'] && rPr['w:sz']['w:val'] > 24,
+ *   style: 'Title',
+ *  },
+ * // find all paragraphs that are strikethrough text and make it a Comment
+ *  {
+ *   markup: 'w:strike',
+ *   style: 'Comment',
+ *  },
+ * ]
+ * ```
+ *
+ * @param options - Plugin options.
+ *
+ * @returns A reoff plugin.
+ */
+export default function reoffMarkupToStyle(
+  options: Options = [
+    {
+      markup: 'w:b',
+      style: 'Heading 1',
+    },
+    {
+      markup: 'w:i',
+      style: 'Heading 2',
+    },
+  ],
+): ReturnType<Plugin<[Options?] | void[], Root, Root>> {
+  return (tree: Root) => {
+    return markupToStyle(tree as Root, options) as Root
+  }
+}

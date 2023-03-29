17dd7809ccecef676768c56115278ad3b10104ee
Thomas F. K. Jorna <hello@tefkah.com>
2023-03-29 21:17:41 +0200

    fix: update converter

--- apps/converter-frontend/utils/converters/docxToTex.ts
+++ apps/converter-frontend/utils/converters/docxToTex.ts
@@ -7,11 +7,12 @@ import { unified } from 'unified'
 import { docxToVFile } from 'docx-to-vfile'
 import reoffUnifiedLatex from 'reoff-unified-latex'
 import unifedLatexStringify from 'unified-latex-stringify'
+import reoffMarkupToStyle from 'reoff-markup-to-style'
 
 export async function docxToTexConverter(
   input: ArrayBuffer,
   options: {
-    citationType?: 'mendeley' | 'native' | 'citavi' | 'zotero' | 'endnote'
+    citationType?: 'mendeley' | 'word' | 'citavi' | 'zotero' | 'endnote'
     url?: string
     mailto?: string
     // preamble?: PreambleCommand[]
@@ -22,11 +23,22 @@ export async function docxToTexConverter(
   const uint = new Uint8Array(input)
   const vfile = await docxToVFile(uint)
 
+  let before
+  let after
   const proc = unified()
     .use(reoffParse)
     .use(reoffClean, {
       rPrRemoveList: ['w:lang', 'w:shd', 'w:szCs', 'w:kern', 'w:rFonts', 'w:noProof'],
     })
+    .use(() => (tree) => {
+      console.log({ prevTree: JSON.parse(JSON.stringify(tree)) })
+    })
+    .use(reoffMarkupToStyle, [
+      {
+        markup: ['w:b'],
+        style: 'Heading 1',
+      },
+    ])
     .use(reoffParseReferences, {
       apiUrl:
         process.env.NODE_ENV === 'production'
@@ -34,8 +46,10 @@ export async function docxToTexConverter(
           : process.env.NEXT_PUBLIC_STYLE_DEV_URL || 'http://localhost:8000/api/style',
       mailto,
     })
-    //  @ts-expect-error tbh idk what it's talking about
-    .use(reoffCite, { type: type || 'zotero', bibliography: undefined })
+    .use(reoffCite, { type: type ?? 'zotero' })
+    .use(() => (tree, vfile) => {
+      console.log({ tree: JSON.parse(JSON.stringify(tree)) })
+    })
     .use(reoffUnifiedLatex)
     .use(unifedLatexStringify)
 

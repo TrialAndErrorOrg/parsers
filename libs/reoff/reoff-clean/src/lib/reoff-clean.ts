81d36d76c081a6181078e3aa8d7533ef2f2006f9
Thomas F. K. Jorna <hello@tefkah.com>
2023-05-30 13:52:18 +0200

    fix(reoff-clean): also process footnotes and endnotes

--- libs/reoff/reoff-clean/src/lib/reoff-clean.ts
+++ libs/reoff/reoff-clean/src/lib/reoff-clean.ts
@@ -1,6 +1,7 @@
 import { Root } from 'ooxast'
 import { ooxastUtilRemoveRsid, Options } from 'ooxast-util-remove-rsid'
 import { Plugin } from 'unified'
+import { VFile } from 'vfile'
 
 export function reoffClean(
   options: Options = {
@@ -14,9 +15,25 @@ export function reoffClean(
       'w:noProof',
       'w:color',
     ],
-  }
+  },
 ): ReturnType<Plugin<[Options?] | void[], Root, Root>> {
-  return (tree: Root) => {
-    return ooxastUtilRemoveRsid(tree as Root, options) as Root
+  return (tree: Root, vfile: VFile) => {
+    const cleanedTree = ooxastUtilRemoveRsid(tree as Root, options) as Root
+
+    if (vfile.data.parsed?.['word/footnotes.xml']) {
+      vfile.data.parsed['word/footnotes.xml'] = ooxastUtilRemoveRsid(
+        vfile.data.parsed['word/footnotes.xml'] as Root,
+        options,
+      ) as Root
+    }
+
+    if (vfile.data.parsed?.['word/endnotes.xml']) {
+      vfile.data.parsed['word/endnotes.xml'] = ooxastUtilRemoveRsid(
+        vfile.data.parsed['word/endnotes.xml'] as Root,
+        options,
+      ) as Root
+    }
+
+    return cleanedTree
   }
 }

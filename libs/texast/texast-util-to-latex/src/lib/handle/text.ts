29b7b4d15cdf21a10b7996c4b4abf9b351acf12a
Thomas F. K. Jorna <hello@tefkah.com>
2023-05-30 13:53:44 +0200

    chore: ?

--- libs/texast/texast-util-to-latex/src/lib/handle/text.ts
+++ libs/texast/texast-util-to-latex/src/lib/handle/text.ts
@@ -1,26 +1,19 @@
 import { Text } from 'texast'
-import {
-  Handle,
-  Context,
-  Options,
-  SafeOptions,
-  Parent,
-  BasicHandle,
-} from '../types'
+import { Handle, Context, Options, SafeOptions, Parent, BasicHandle } from '../types'
 import { safe } from '../util/safe'
-//@ts-expect-error
+//@ts-expect-error escape-latex is not typed correctly
 import lxescape from 'escape-latex'
 
 type EscapeMapFn = (
   defaultEscapes: { [key: string]: string },
-  formattingEscapes: { [key: string]: string }
+  formattingEscapes: { [key: string]: string },
 ) => { [key: string]: string }
 type Lescape = (
   texString: string,
   options?: {
     preserveFormatting?: boolean
     escapeMapFn?: EscapeMapFn
-  }
+  },
 ) => string
 const lescape = lxescape as Lescape
 

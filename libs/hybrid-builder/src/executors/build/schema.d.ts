eac75cddf5290bb53427cf2d501113e12ddfddc8
Thomas F. K. Jorna <hello@tefkah.com>
2023-09-29 16:23:48 +0200

    chore: remove hybrid builder

--- libs/hybrid-builder/src/executors/build/schema.d.ts
+++
@@ -1,5 +0,0 @@
-import { SwcExecutorOptions } from '@nx/js/src/utils/schema'
-export interface BuildExecutorSchema extends SwcExecutorOptions {
-  tsConfigESM: string
-  tsConfigCJS: string
-}
--- libs/hybrid-builder/src/generators/hybrid-builder/schema.d.ts
+++
@@ -1,5 +0,0 @@
-export interface HybridBuilderGeneratorSchema {
-  name: string
-  tags?: string
-  directory?: string
-}

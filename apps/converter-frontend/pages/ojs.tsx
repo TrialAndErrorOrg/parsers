bce410471137c27bd4e7e9f8a85e06983e402b09
Thomas F. K. Jorna <hello@tefkah.com>
2026-02-21 15:38:16 +0100

    fix: remove unused apps

--- apps/converter-frontend/pages/ojs.tsx
+++ apps/converter-frontend/pages/ojs.tsx
@@ -1,39 +1,16 @@
-import React from 'react'
-import { Title } from '@mantine/core'
-import Settings from '../components/settings/settings'
-import Conversion from '../components/conversion/conversion'
+import { Title } from "@mantine/core";
+import React from "react";
+import Conversion from "../components/conversion/conversion";
+import Settings from "../components/settings/settings";
 
 export default function Index() {
-  // const [thing, setThing] = useState<ArrayBuffer>()
-  // const [tex, setTex] = useState('')
-  // const [from, setFrom] = useState<'docx' | 'jats'>('docx')
-  // const [to, setTo] = useState<'jats' | 'tex'>('tex')
+	return (
+		<>
+			<Title> Convert with OJS help</Title>
 
-  // useEffect(() => {
-  //   if (!thing) return
+			<Settings />
 
-  //   fetch('/api/jats-to-tex', {
-  //     method: 'post',
-  //     headers: {
-  //       Accept: 'application/json, text/plain, */*',
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({ response: Buffer.from(thing).toString() }),
-  //   })
-  //     .then((res) => res.text())
-  //     .then((res) => setTex(res))
-  //     .catch((e) => {
-  //       console.error(e)
-  //       setTex(e)
-  //     })
-  // }, [thing])
-  return (
-    <>
-      <Title> Convert with OJS help</Title>
-
-      <Settings />
-
-      <Conversion />
-    </>
-  )
+			<Conversion />
+		</>
+	);
 }
